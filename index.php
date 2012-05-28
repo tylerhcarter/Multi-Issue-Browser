df0b06380e7c03e746a8001ea5a1aa1dda06ca0f
Tyler C <tyler@chacha102.com>
2012-05-27 17:07:01 -0700

    Added ability to make multiple repositories

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use GitHub\API\Repo\Issue;
 use GitHub\API\AuthenticationException;
 
 include_once 'config.php';
+include_once 'lib.php';
 
 include 'view/column.php';
 include 'view/section.php';
@@ -19,40 +20,53 @@ $issues = new Issue();
 $issues->setCredentials( new Authentication\Basic( GITHUB_USER_NAME, GITHUB_USER_PASS));
 $issues->login();
 
-$milestone = new Milestone();
-$milestone->title = "First Milestone";
-
-$unlabeled = new Issue_Column;
-$unlabeled->title = "Unlabeled";
-
-$columns = array(
+$grab = array(
+	8 => "1.1",
 );
 
-$all = $issues->all("AppThemes", "Vantage", array(), array( 'sort' => 'updated' ), 1, 100 );
-$all = json_decode($all);
-foreach( $all as $issue ){
-	if( count($issue->labels) == 0 ){
-		$unlabeled->add_row($issue);
-	}
+foreach( $grab as $id => $title ){
 
-	foreach( $issue->labels as $label ){
-		if( !isset($columns[$label->name])){
-			$new = new Issue_Column();
-			$new->title = $label->name;
-			$new->color = $label->color;
-			$columns[$label->name] = $new;
-		}
+	$milestone = new Milestone();
+	$milestone->title = "Vantage " . $title;
 
-		$columns[$label->name]->add_row( $issue );
-	}
+	fill_milestone( $issues, $milestone, $id );
+
+	$milestones[] = $milestone;
 }
 
-foreach( $columns as $column )
-	$milestone->add_column($column);
+function fill_milestone( $issues, $milestone, $id ){
 
-$milestone->add_column($unlabeled);
+	$unlabeled = new Issue_Column;
+	$unlabeled->title = "Unlabeled";
 
+	$columns = array(
+	);
 
+	$all = $issues->all("AppThemes", "Vantage", array( 'milestone' => $id ), array( 'sort' => 'updated' ), 1, 100 );
+	$all = json_decode($all);
+	foreach( $all as $issue ){
+		if( count($issue->labels) == 0 ){
+			$unlabeled->add_row($issue);
+		}
+
+		foreach( $issue->labels as $label ){
+			if( !isset($columns[$label->name])){
+				$new = new Issue_Column();
+				$new->title = $label->name;
+				$new->color = $label->color;
+				$columns[$label->name] = $new;
+			}
+
+			$columns[$label->name]->add_row( $issue );
+		}
+	}
+
+	foreach( $columns as $column )
+		$milestone->add_column($column);
+
+	$milestone->add_column($unlabeled);
+
+}
 ?>
 <html>
 	<head>
@@ -60,6 +74,18 @@ $milestone->add_column($unlabeled);
 	</head>
 	<body>
 		<div class="container">
-			<?php $milestone->display(); ?>
+			<?php foreach( $milestones as $milestone )
+				$milestone->display();
+			?>
+		</div>
+		<div class="info-bar">
+			<div class="content">
+			<?php 
+			$headers = $issues->getTransport()->getJournal()->getLast()->getResponse()->getHeaders();
+			$rate_limit = get_header( 'X-RateLimit-Remaining', $headers );
+			?>
+			Rate Limit: <?php echo $rate_limit . "/5000"; ?>
+			</div>
+		</div>
 	</body>
 </html>
